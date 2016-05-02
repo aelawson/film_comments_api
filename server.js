773685d43e793f4b3a5ae5adb544a4434615910f
Andrew Lawson <aeriklawson@gmail.com>
2016-05-01 22:47:06 -0400

    Add sequelize, pg, pg-hstore libraries. Add sequelize init code to project. Add sequelize cli config file to project root.

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var http = require('http');
 var express = require('express');
 var socketio = require('socket.io');
 var redis = require('redis-connection');
+var Sequelize = require('sequelize');
 
+// Db Settings
+DB_HOST = "localhost";
+DB_DATABASE = "database";
+DB_USERNAME "root";
+DB_PASSWORD = "";
+DB_DIALECT = "postgres";
+
+// Ports
 var portApi = process.env.PORT_API || 5000;
 var portSocket = process.env.PORT || 8080;
 
@@ -12,22 +21,62 @@ var router = express.Router();
 var io = socketio.listen(server);
 var redisClient = redis();
 
-// Routes
-router.get('/', function(req, res) {
-    res.json({ message: "Default response." });
-});
+// Initialization
+initSequelize();
+initApi();
+initSocket();
 
-app.use('/api', router);
-app.listen(portApi);
-console.log('Listening on HTTP Port: ' + portApi);
-
-// Socket.IO
-server.listen(portSocket);
-console.log('Listening on Socket Port: ' + portSocket);
-io.on('connection', function(connection) {
-    console.log("Client connected successfully.");
-    connection.on('timestamp', function(data) {
-        redisClient.publish('timestamp', data);
-        console.log("Published: " + data);
+// Initialize sequelize.
+function initSequelize() {
+    var sequelize = new sequelize(
+        DB_DATABASE,
+        DB_USERNAME,
+        DB_PASSWORD, {
+        host: DB_HOST,
+        dialect: DB_DIALECT
     });
-});
+    sequelize.sync().then(function() {
+        return User.create({
+            username: 'janedoe',
+            birthday: new Date(1980, 6, 20)
+        });
+    }).then(function(jane) {
+        console.log(jane.get({
+            plain: true
+        }));
+    });
+}
+
+// Initialize the API listener and router.
+function initApi() {
+    app.use('/api', router);
+    app.listen(portApi);
+    // GET
+    router.get('/', function(req, res) {
+        // To do.
+    });
+    // POST
+    router.post('/add_comment', function(req, res) {
+        // To do.
+    });
+    router.post('/delete_comment', function(req, res) {
+        // To do.
+    });
+    router.post('/get_comments', function(req, res) {
+        // To do.
+    });
+    console.log('Listening on HTTP Port: ' + portApi);
+}
+
+// Initialize the socket listener.
+function initSocket() {
+    server.listen(portSocket);
+    io.on('connection', function(connection) {
+        console.log("Client connected successfully.");
+        connection.on('timestamp', function(data) {
+            redisClient.publish('timestamp', data);
+            console.log("Published: " + data);
+        });
+    });
+    console.log('Listening on Socket Port: ' + portSocket);
+}
